622f367f4c8e1245152faed3675cb2a8961c1e1b
Nikita Skorodumov <mr-zerx@yandex.ru>
2017-12-11 16:42:27 +0500

    Моя сборка

---
+++ build/js/about.js
@@ -0,0 +1,16 @@
+webpackJsonp([3],[
+/* 0 */,
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+eval("\n\n__webpack_require__(0);\n\n__webpack_require__(2);\n\n//////////////////\n// WEBPACK FOOTER\n// ./source/pages/about/about.js\n// module id = 1\n// module chunks = 3\n\n//# sourceURL=webpack:///./source/pages/about/about.js?");
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports) {
+
+eval("// removed by extract-text-webpack-plugin\n\n//////////////////\n// WEBPACK FOOTER\n// ./source/pages/about/about.scss\n// module id = 2\n// module chunks = 3\n\n//# sourceURL=webpack:///./source/pages/about/about.scss?");
+
+/***/ })
+],[1]);
---
+++ build/js/my-works.js
@@ -0,0 +1,18 @@
+webpackJsonp([0],{
+
+/***/ 7:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+eval("\n\n__webpack_require__(0);\n\n__webpack_require__(8);\n\n//////////////////\n// WEBPACK FOOTER\n// ./source/pages/my-works/my-works.js\n// module id = 7\n// module chunks = 0\n\n//# sourceURL=webpack:///./source/pages/my-works/my-works.js?");
+
+/***/ }),
+
+/***/ 8:
+/***/ (function(module, exports) {
+
+eval("// removed by extract-text-webpack-plugin\n\n//////////////////\n// WEBPACK FOOTER\n// ./source/pages/my-works/my-works.scss\n// module id = 8\n// module chunks = 0\n\n//# sourceURL=webpack:///./source/pages/my-works/my-works.scss?");
+
+/***/ })
+
+},[7]);
